0aab9e0cae1d31fa2e2e6b40dc7175f58c8a9154
tankist2002 <72762534+tankist2002@users.noreply.github.com>
2021-05-07 17:13:03 +0300

    Update polynome.py

--- polynome.py
+++ polynome.py
@@ -31,38 +31,7 @@ def DEG_P_N(listSt):
     return listSt[0]
 
   
-def SUB_PP_P(stroka1,stroka2):
-    #Семёнов Михаил
-    #Вычитание многочленов
-    i = 0
-    rez = [] #создаём пустой список
-    while (i != len(stroka1) and j != len(stroka2)):#условие прохода по двум многочленам
-        while(i != ' ' and i < len(stroka1)):#пока не встретили пробел или не закончилась строка
-            i += 1 #увеличиваем счётчик
-        if stroka1[i]==' ': #если дошли до пробела
-            mnog1 = list(stroka1[count1:i]) #создаём список ,состоящий из цифр числа коэффицента полинома
-            k = 0
-            for k in range(len(mnog1)):  #проход по созданному списку
-                if mnog1[i] != ' ' and mnog1[i] != '/': #если значения списка цифры
-                    mnog1[i] = int(mnog1[i]) # переводим символы в цифры
-            i = i + 1 #увеличиваем счётчик
-            count1 = i
-        while(j != ' ' and j < len(stroka2)): #аналогичные действия для второго полинома
-            j += 1
-        if stroka2[j] == ' ':
-            mnog2 = list(stroka2[count2:j])
-            k=0
-            for k in range(len(mnog2)):
-                if mnog2[i] != ' ' and mnog2[i] != '/':
-                    mnog2[i] = int(mnog2[i])
-            j = j + 1
-            count2 = j
-        if (i != len(stroka1)) or (j != len(stroka2)): #если не дошли до конца
-            rez = rez + SUBB_QQ_Q(mnog1,mnog2) + [' '] #прибавляем к списку разность коэффицентов полиномов
-        else :
-            rez = rez + SUBB_QQ_Q(mnog1,mnog2) #тоже самое ,только без пробела после последнего элемента
 
-    result="".join(map(str,rez)) #склеивеаем в строку
 
 def ADD_PP_P(x, y):
     # Кривоконь Максим
@@ -91,6 +60,15 @@ def ADD_PP_P(x, y):
         res = res + ADD_QQ_Q((prdx[i]+"/1"), (prdy[i]+"/1")) + " "  # Получаем итоговый результат сложения
         i = i + 1
     return res
+
+
+def SUB_PP_P(list1,list2,stepen1,stepen2)
+    for i in range(len(list2)):
+        if list2[i][0][0] == '-':
+            list2[i][0] = list2[i][0][1:]
+        else:
+            list2[i][0] = ['-'] + list2[i][0]
+    return(ADD_PP_P(list1,list2,stepen1,stepen2))
 
 def MUL_Pxk_P(polynome2,k):
     #Дашкин Дамир

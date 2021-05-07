b10cb7ab27faf67e40d88b68a9a1b2fe645a7da7
Saveliy344 <guryanov.saveliy2014@yandex.ru>
2021-05-08 00:07:25 +0300

    Обновление многочленов

--- polynome.py
+++ polynome.py
@@ -1,68 +1,78 @@
 import natural as nat
+import rational as rat
+
 
 def MUL_PQ_P(P, Q):
-    for index in range(0, P[0]):
-        P[1][index] = MUL_PQ_P(P[1][index], Q)
+    # Гурьянов Савелий
+    # Каждый коэффициент многочлена циклично умножается на переданное рациональное число
+    for index in range(len(P[0])):
+        P[0][index] = rat.MUL_QQ_Q(P[0][index], Q)
     return P
-# Гурьянов Савелий
-# Каждый коэффициент многочлена циклично умножается на переданное рациональное число
 
 
 def GCF_PP_P(P1, P2):
+    # Гурьянов Савелий
+    # Реализуется алгоритм Евклида для многочленов: пока степень меньшего многочлена неотрицательна,
+    # в больший многочлен переписывается меньший, а в меньший - остаток от деления большего на меньший
     if DEG_P_N(P1) >= DEG_P_N(P2):
         while (DEG_P_N(P2) != 0):
             P1, P2 = P2, MOD_PP_P(P1, P2)
         return P1
     else:
         return GCF_PP_P(P2, P1)
-# Гурьянов Савелий
-# Реализуется алгоритм Евклида для многочленов: пока степень меньшего многочлена неотрицательна,
-# в больший многочлен переписывается меньший, а в меньший - остаток от деления большего на меньший
+
+
+def NMR_P_P(P):
+    # Гурьянов Савелий
+    # Многочлен делится на НОД многочлена и производной
+    P = DIV_PP_P(P, GCF_PP_P(P, DER_P_P(P)))
 
 
 def LED_P_Q(listCff):
-# Пекло Елизавета
-#Нахождение старшего коэффициента многочлена
+    # Пекло Елизавета
+    # Нахождение старшего коэффициента многочлена
     return listCff[0]
 
+
 def DEG_P_N(listSt):
-# Пекло Елизавета
-# Получение степени многочлена
+    # Пекло Елизавета
+    # Получение степени многочлена
     return listSt[0]
 
-  
-def SUB_PP_P(stroka1,stroka2):
-    #Семёнов Михаил
-    #Вычитание многочленов
+
+def SUB_PP_P(stroka1, stroka2):
+    # Семёнов Михаил
+    # Вычитание многочленов
     i = 0
-    rez = [] #создаём пустой список
-    while (i != len(stroka1) and j != len(stroka2)):#условие прохода по двум многочленам
-        while(i != ' ' and i < len(stroka1)):#пока не встретили пробел или не закончилась строка
-            i += 1 #увеличиваем счётчик
-        if stroka1[i]==' ': #если дошли до пробела
-            mnog1 = list(stroka1[count1:i]) #создаём список ,состоящий из цифр числа коэффицента полинома
+    rez = []  # создаём пустой список
+    while (i != len(stroka1) and j != len(stroka2)):  # условие прохода по двум многочленам
+        while (i != ' ' and i < len(stroka1)):  # пока не встретили пробел или не закончилась строка
+            i += 1  # увеличиваем счётчик
+        if stroka1[i] == ' ':  # если дошли до пробела
+            mnog1 = list(stroka1[count1:i])  # создаём список ,состоящий из цифр числа коэффицента полинома
             k = 0
-            for k in range(len(mnog1)):  #проход по созданному списку
-                if mnog1[i] != ' ' and mnog1[i] != '/': #если значения списка цифры
-                    mnog1[i] = int(mnog1[i]) # переводим символы в цифры
-            i = i + 1 #увеличиваем счётчик
+            for k in range(len(mnog1)):  # проход по созданному списку
+                if mnog1[i] != ' ' and mnog1[i] != '/':  # если значения списка цифры
+                    mnog1[i] = int(mnog1[i])  # переводим символы в цифры
+            i = i + 1  # увеличиваем счётчик
             count1 = i
-        while(j != ' ' and j < len(stroka2)): #аналогичные действия для второго полинома
+        while (j != ' ' and j < len(stroka2)):  # аналогичные действия для второго полинома
             j += 1
         if stroka2[j] == ' ':
             mnog2 = list(stroka2[count2:j])
-            k=0
+            k = 0
             for k in range(len(mnog2)):
                 if mnog2[i] != ' ' and mnog2[i] != '/':
                     mnog2[i] = int(mnog2[i])
             j = j + 1
             count2 = j
-        if (i != len(stroka1)) or (j != len(stroka2)): #если не дошли до конца
-            rez = rez + SUBB_QQ_Q(mnog1,mnog2) + [' '] #прибавляем к списку разность коэффицентов полиномов
-        else :
-            rez = rez + SUBB_QQ_Q(mnog1,mnog2) #тоже самое ,только без пробела после последнего элемента
+        if (i != len(stroka1)) or (j != len(stroka2)):  # если не дошли до конца
+            rez = rez + SUBB_QQ_Q(mnog1, mnog2) + [' ']  # прибавляем к списку разность коэффицентов полиномов
+        else:
+            rez = rez + SUBB_QQ_Q(mnog1, mnog2)  # тоже самое ,только без пробела после последнего элемента
 
-    result="".join(map(str,rez)) #склеивеаем в строку
+    result = "".join(map(str, rez))  # склеивеаем в строку
+
 
 def ADD_PP_P(x, y):
     # Кривоконь Максим
@@ -71,7 +81,7 @@ def ADD_PP_P(x, y):
     prdx = x.split(' ')
     prdy = y.split(' ')
     i = len(prdx)
-    prdx.reverse() 
+    prdx.reverse()
     prdy.reverse()
     if (len(prdx) < len(prdy)):  # Если кол-во коэффициентов в первой переменной меньше, чем во второй, то
         while (i != len(prdy)):  # записываем их нулями - "00"
@@ -88,14 +98,14 @@ def ADD_PP_P(x, y):
     print(prdx)
     print(prdy)
     while (i != len(prdx)):
-        res = res + ADD_QQ_Q((prdx[i]+"/1"), (prdy[i]+"/1")) + " "  # Получаем итоговый результат сложения
+        res = res + ADD_QQ_Q((prdx[i] + "/1"), (prdy[i] + "/1")) + " "  # Получаем итоговый результат сложения
         i = i + 1
     return res
 
-def MUL_Pxk_P(polynome2,k):
-    #Дашкин Дамир
-    #Умножение многочлена на x^k
+
+def MUL_Pxk_P(polynome2, k):
+    # Дашкин Дамир
+    # Умножение многочлена на x^k
     for i in range(len(polynome2)):
         polynome2[i] = nat.ADD_NN_N(polynome2[i], k)
     return polynome2
-
